7491a3a040430a7bfa21d525831b83b0cd018f87
GUYwithPTSD <ivandarma3000@gmail.com>
2025-10-31 21:24:53 +0700

    commit 5 - align center +tambahan folder asset

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,21 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

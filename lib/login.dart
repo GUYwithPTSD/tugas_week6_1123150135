14767c69b3b7d0a2a7b50fdf26bf39141a415bc3
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-01 23:07:24 +0700

    commit 14 - membuat textfield password

--- lib/login.dart
+++ lib/login.dart
@@ -31,8 +31,8 @@ class Login extends StatelessWidget {
                 TextField(
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                    labelText: 'email',
-                    hintText: 'masukkan email anda',
+                    labelText: 'Email',
+                    hintText: 'Masukkan email anda',
                     prefixIcon: Icon(Icons.email_outlined),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(12.0)),
@@ -45,6 +45,16 @@ class Login extends StatelessWidget {
                       borderSide: BorderSide(color: Colors.blue, width: 2.0),
                       borderRadius: BorderRadius.all(Radius.circular(12.0)),
                     ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukkan password anda',
+                    prefixIcon: Icon(Icons.lock_outlined),
+                    suffixIcon: Icon(Icons.visibility_outlined),
                   ),
                 ),
               ],
